3437c6fa2cc11d9b7fe5d9db3def74cb1e503432
Finn <jm89.kim@gmail.com>
2023-09-19 09:39:15 +0900

    First Commit

---
+++ DRV_Main.py
@@ -0,0 +1,17 @@
+#### Module Define ######################################
+from Func._Main_Func import * 
+#### 0. 초기값 입력 ########################################
+# a. 분석할 날짜
+#   [0] : Start Date / [1] End Date
+Date_List = ['20230917', '20230918']
+# b. User List
+User_List = ['Bryan']
+# c. Plug type List
+Plug_List = ['tlk']
+# d. Simulation Mode Type
+#   0 : User, 1 : Plug, ( 2 : User & Plug X)
+SimMode = 0
+
+#### 3.Simulation Start ##################################
+Start_Simulation(SimMode, Date_List, User_List, Plug_List)
+
